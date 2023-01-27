7b4f048e7de40e8eb49654461e18ccc58a82a6a6
Rohan <rohankumarchaudhary67@gmail.com>
2023-01-28 00:52:31 +0530

    Add the product page

--- pages/sneakers.js
+++ pages/sneakers.js
@@ -10,7 +10,7 @@ const Sneakers = () => {
                     <div className="flex flex-wrap justify-center -m-4">
                         <Link href={'/product/seme'} className="lg:w-1/4 md:w-1/2 w-full shadow-xl m-3 p-3"><div>
                             <a className="h-[36vh]">
-                                <img alt="ecommerce" className="" src="https://m.media-amazon.com/images/I/71blOkj9gLL._UX695_.jpg"/>
+                                <img alt="ecommerce" className="" src="https://m.media-amazon.com/images/I/61rNNecDMmL._UX695_.jpg"/>
                             </a>
                             <div className="mt-4 text-center">
                                 <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Sneakers</h3>
